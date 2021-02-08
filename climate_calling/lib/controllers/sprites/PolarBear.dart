1fcd1604e215907dda1650092f2a140b2d709980
GiantSweetroll <33750894+GiantSweetroll@users.noreply.github.com>
2021-02-08 23:19:18 +0700

    Create PolarBear.dart

---
+++ climate_calling/lib/controllers/sprites/PolarBear.dart
@@ -0,0 +1,8 @@
+import 'package:climate_calling/controllers/sprites/NPCSprite.dart';
+
+class PolarBear extends NPCSprite {
+  //Constructor
+  //TODO: Load player animations
+  PolarBear() : super() {
+  }
+}
