6f592fcfafc5e42765d5f7fa20b22d1978d56cd0
GiantSweetroll <33750894+GiantSweetroll@users.noreply.github.com>
2021-02-08 23:26:30 +0700

    Added a list that will store reference to NPCSprite objects that are being guided by the player

--- climate_calling/lib/shared/globals.dart
+++ climate_calling/lib/shared/globals.dart
@@ -1,3 +1,5 @@
+import 'package:climate_calling/controllers/sprites/NPCSprite.dart';
 import 'package:climate_calling/flutter_screens/FlutterScreenManager.dart';
 
-FlutterScreenManager flutterScreenManager = FlutterScreenManager();
+FlutterScreenManager flutterScreenManager = FlutterScreenManager();   //Screen manager that manages betweeen flutter widgets with flame widgets
+List<NPCSprite> guidedNPCs = List();    //Variable to allow multiple NPCs being guided by the player
