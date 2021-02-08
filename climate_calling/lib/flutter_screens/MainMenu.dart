90377fa95181a0eccfd32f1077475aa08eb172c2
GiantSweetroll <33750894+GiantSweetroll@users.noreply.github.com>
2021-02-08 23:46:53 +0700

    Create MainMenu.dart

---
+++ climate_calling/lib/flutter_screens/MainMenu.dart
@@ -0,0 +1,28 @@
+import "package:flutter/material.dart";
+
+class MainMenuScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //TODO: Create main menu widget tree
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Flexible(
+          flex: 2,
+          child: Container(), //TODO: Replace with logo widget here
+        ),
+        Flexible(
+          flex: 1,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              //TODO: Put the first button widgets here
+              SizedBox(width: 10,),
+              //TODO: Put the second button widgets here
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
