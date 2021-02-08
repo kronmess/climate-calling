6d2dcb32af8de9a3fe3b2ef623854a36aa617753
GiantSweetroll <33750894+GiantSweetroll@users.noreply.github.com>
2021-02-08 15:02:48 +0700

    Create FlutterScreenManager.dart

---
+++ climate_calling/lib/flutter_screens/FlutterScreenManager.dart
@@ -0,0 +1,32 @@
+import "package:flutter/material.dart";
+import 'package:flutter/services.dart';
+
+class FlutterScreenManager extends StatefulWidget {
+  @override
+  _FlutterScreenManagerState createState() => _FlutterScreenManagerState();
+}
+
+class _FlutterScreenManagerState extends State<FlutterScreenManager> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    SystemChrome.setEnabledSystemUIOverlays([]);    //Remove bottom navigation bars
+    SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeLeft]);   //Make the app to use landscape by default
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: Stack(
+          children: [
+            //TODO: Add the Flame and flutter widgets here
+          ],
+        ),
+      ),
+    );
+  }
+}
