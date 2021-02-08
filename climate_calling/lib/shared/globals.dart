14f4fccde162e1eeb826ef33540c50df376c96cd
GiantSweetroll <33750894+GiantSweetroll@users.noreply.github.com>
2021-02-09 01:31:16 +0700

    Added a ScreenState object

--- climate_calling/lib/shared/globals.dart
+++ climate_calling/lib/shared/globals.dart
@@ -1,5 +1,7 @@
 import 'package:climate_calling/controllers/sprites/NPCSprite.dart';
 import 'package:climate_calling/flutter_screens/FlutterScreenManager.dart';
+import 'package:climate_calling/shared/ScreenState.dart';
 
 FlutterScreenManager flutterScreenManager = FlutterScreenManager();   //Screen manager that manages betweeen flutter widgets with flame widgets
 List<NPCSprite> guidedNPCs = List();    //Variable to allow multiple NPCs being guided by the player
+ScreenState screenState = ScreenState.kMainMenu;    //Keeps track of the screen states.
