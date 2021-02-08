75fbf4a5e76ed9d5a6c9e2c667a6f7f7f066a58f
GiantSweetroll <33750894+GiantSweetroll@users.noreply.github.com>
2021-02-09 01:31:19 +0700

    Create GameServices.dart

---
+++ climate_calling/lib/services/GameServices.dart
@@ -0,0 +1,7 @@
+import 'package:climate_calling/shared/ScreenState.dart';
+import 'package:climate_calling/shared/globals.dart';
+
+void updateScreenState(ScreenState newState, {Function callback}) {
+  screenState = newState;
+  callback();
+}
