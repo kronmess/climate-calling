d796132d381aca3d54a90ce576bf7bcf0ae18452
GiantSweetroll <33750894+GiantSweetroll@users.noreply.github.com>
2021-02-08 23:03:30 +0700

    Added method to check if a sprite is directly on top of another sprite

--- climate_calling/lib/services/SpriteServices.dart
+++ climate_calling/lib/services/SpriteServices.dart
@@ -7,7 +7,6 @@ class SpriteServices {
    * Uses AABB collision detection to detect if 2 sprites collide with one another
    */
   static bool hasCollision(BaseSprite sp1, BaseSprite sp2) {
-
     AnimationComponent ac1 = sp1.getAnimationComponent();
     AnimationComponent ac2 = sp2.getAnimationComponent();
 
@@ -15,5 +14,15 @@ class SpriteServices {
             ac1.x + ac2.width > ac2.x &&
             ac1.y < ac2.y + ac2.height &&
             ac1.y + ac1.height > ac2.y);
+  }
+
+  /**
+   * Checks if the first sprite is on top of the second sprite
+   */
+  static bool isDirectlyOnTop(BaseSprite sp1, BaseSprite sp2) {
+    AnimationComponent ac1 = sp1.getAnimationComponent();
+    AnimationComponent ac2 = sp2.getAnimationComponent();
+
+    return ac1.y + ac1.height == ac2.y;
   }
 }
