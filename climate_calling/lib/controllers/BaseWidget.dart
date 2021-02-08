bb86b409060cda3c1e48f6b133c7354a6e31878e
GiantSweetroll <33750894+GiantSweetroll@users.noreply.github.com>
2021-02-08 15:18:19 +0700

    Update BaseWidget.dart

--- climate_calling/lib/controllers/BaseWidget.dart
+++ climate_calling/lib/controllers/BaseWidget.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/cupertino.dart';
 
 abstract class BaseWidget {
+  /**
+   * Renders objects unto the screen
+   */
   void render(Canvas canvas);
+  /**
+   * Performs any update in the data
+   */
   void update();
+  /**
+   * Recalculate the sizing of objects
+   */
   void resize(Size size);
+  /**
+   * Operation to be done when the user taps on the screen
+   */
   void onTapDown(TapDownDetails details, Function fn);
 }
