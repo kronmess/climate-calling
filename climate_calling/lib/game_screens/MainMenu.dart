6545eaf24981d679d78cb961c078ced5bb818694
GiantSweetroll <33750894+GiantSweetroll@users.noreply.github.com>
2021-02-09 01:31:22 +0700

    Create MainMenu.dart

---
+++ climate_calling/lib/game_screens/MainMenu.dart
@@ -0,0 +1,29 @@
+import 'dart:ui';
+
+import 'package:climate_calling/game_screens/BaseScreen.dart';
+import 'package:flutter/src/gestures/tap.dart';
+
+class MainMenuScreen extends BaseScreen {
+
+  //Overridden Methods
+  @override
+  void onTapDown(TapDownDetails details) {
+      // TODO: implement onTapDown
+    }
+  
+    @override
+    void render(Canvas canvas) {
+      // TODO: implement render
+    }
+  
+    @override
+    void resize(Size size) {
+    // TODO: implement resize
+  }
+
+  @override
+  void update() {
+    // TODO: implement update
+  }
+
+}
