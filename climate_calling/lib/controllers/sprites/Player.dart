075cf5905c32420f1af5683b5ad671ad16c69e44
GiantSweetroll <33750894+GiantSweetroll@users.noreply.github.com>
2021-02-08 23:19:09 +0700

    Added a field that tells if the player is currently guiding an npc

--- climate_calling/lib/controllers/sprites/Player.dart
+++ climate_calling/lib/controllers/sprites/Player.dart
@@ -3,6 +3,8 @@ import 'dart:ui';
 import 'package:climate_calling/controllers/sprites/BaseSprite.dart';
 
 class Player extends BaseSprite {
+  //Fields
+  bool _guideMode = false;    //Tells if the player is currently guiding another sprite
 
   //Constructor
   //TODO: Load player animations
