30f4d0c74f11b2c50da9eb0dd81b5234373e164d
GiantSweetroll <33750894+GiantSweetroll@users.noreply.github.com>
2021-02-09 08:50:30 +0700

    Create Background.dart

---
+++ climate_calling/lib/game_screens/Background.dart
@@ -0,0 +1,36 @@
+import 'dart:ui';
+
+import 'package:climate_calling/controllers/BaseWidget.dart';
+import 'package:flame/components/component.dart';
+import 'package:flame/sprite.dart';
+import 'package:flutter/src/gestures/tap.dart';
+
+class Background extends BaseWidget {
+  //Fields
+  SpriteComponent _bgSprite;
+
+  //Constructor
+  Background(String src) {
+    this._bgSprite = SpriteComponent.fromSprite(0, 0, Sprite(src));
+  }
+
+  //Overridden Methods
+  @override
+  void onTapDown(TapDownDetails details, Function fn) {
+      // TODO: implement onTapDown
+    }
+  
+    @override
+    void render(Canvas canvas) {
+     this._bgSprite.render(canvas);
+    }
+  
+    @override
+    void resize(Size size) {
+    this._bgSprite.width = size.width;
+    this._bgSprite.height = size.height;
+  }
+
+  @override
+  void update() {}
+}
