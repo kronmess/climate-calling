7215b419a2f46779a1368fd2f7f04c467e117542
GiantSweetroll <33750894+GiantSweetroll@users.noreply.github.com>
2021-02-10 13:32:29 +0700

    Create ArcticLevel.dart

---
+++ climate_calling/lib/game_screens/levels/ArcticLevel.dart
@@ -0,0 +1,11 @@
+import 'package:climate_calling/game_screens/levels/BaseLevel.dart';
+
+class ArcticLevel extends BaseLevel {
+
+  //Overridden Methods
+  @override
+  bool isLevelFinished() {
+    // TODO: implement isLevelFinished
+    return false;
+  }
+}
