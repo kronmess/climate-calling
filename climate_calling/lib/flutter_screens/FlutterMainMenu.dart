faaa1c3aa352df4283e2b059c49888291c44d37d
kronmess <vincentiusgabriel@yahoo.com>
2021-02-10 10:28:46 +0700

    replaced text widgets with button widgets

--- climate_calling/lib/flutter_screens/FlutterMainMenu.dart
+++ climate_calling/lib/flutter_screens/FlutterMainMenu.dart
@@ -5,36 +5,59 @@ class FlutterMainMenuScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     //TODO: Create main menu widget tree
     return Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Flexible(
-            flex: 2,
-            child: Icon(Icons.account_circle, size: 300,), //TODO: Replace with logo here
-          ),
-          SizedBox(height: 180,),
-          Flexible(
-            flex: 1,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                //TODO: Replace with first button widget
-                Text(
-                  "Start", 
-                  style: TextStyle(
-                    fontSize: 20,
-                  ),
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Flexible(
+          flex: 2,
+          child: Icon(
+            Icons.account_circle,
+            size: 300,
+          ), //TODO: Replace with logo here
+        ),
+        SizedBox(
+          height: 180,
+        ),
+        Flexible(
+          flex: 1,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              //TODO: Replace with first button widget
+              TextButton(
+                style: ButtonStyle(
+                  overlayColor: MaterialStateColor.resolveWith(
+                      (states) => Colors.transparent),
+                  foregroundColor:
+                      MaterialStateProperty.all<Color>(Colors.black),
                 ),
-                //TODO: Replace with second button widget
-                Text(
-                  "Game Settings",
-                  style: TextStyle(
-                    fontSize: 20,
-                  ),
+                child: Text(
+                  'Start',
+                  style: TextStyle(fontSize: 20),
                 ),
-              ],
-            ),
+                onPressed: () {
+                  print('test');
+                },
+              ),
+              //TODO: Replace with second button widget
+              TextButton(
+                style: ButtonStyle(
+                  overlayColor: MaterialStateColor.resolveWith(
+                      (states) => Colors.transparent),
+                  foregroundColor:
+                      MaterialStateProperty.all<Color>(Colors.black),
+                ),
+                child: Text(
+                  'Game Settings',
+                  style: TextStyle(fontSize: 20),
+                ),
+                onPressed: () {
+                  print('test');
+                },
+              )
+            ],
           ),
-        ],
-      );
+        ),
+      ],
+    );
   }
 }
