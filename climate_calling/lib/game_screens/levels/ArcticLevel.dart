8ed48caf8296f55c3d854afb334e959d56901164
GiantSweetroll <33750894+GiantSweetroll@users.noreply.github.com>
2021-02-10 13:33:27 +0700

    Added missing overrides

--- climate_calling/lib/game_screens/levels/ArcticLevel.dart
+++ climate_calling/lib/game_screens/levels/ArcticLevel.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:climate_calling/game_screens/levels/BaseLevel.dart';
+import 'package:flutter/src/gestures/tap.dart';
 
 class ArcticLevel extends BaseLevel {
 
@@ -7,5 +10,25 @@ class ArcticLevel extends BaseLevel {
   bool isLevelFinished() {
     // TODO: implement isLevelFinished
     return false;
+  }
+
+  @override
+  void onTapDown(TapDownDetails details) {
+      // TODO: implement onTapDown
+    }
+  
+    @override
+    void render(Canvas canvas) {
+      // TODO: implement render
+    }
+  
+    @override
+    void resize(Size size) {
+    // TODO: implement resize
+  }
+
+  @override
+  void update() {
+    // TODO: implement update
   }
 }
