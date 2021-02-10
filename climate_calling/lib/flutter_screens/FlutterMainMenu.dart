27b6cd7f1fff80cb64414a4ed367741d7bb63415
GiantSweetroll <33750894+GiantSweetroll@users.noreply.github.com>
2021-02-10 07:28:18 +0700

    Added temporary widgets in place of the logo and buttons

--- climate_calling/lib/flutter_screens/FlutterMainMenu.dart
+++ climate_calling/lib/flutter_screens/FlutterMainMenu.dart
@@ -4,25 +4,37 @@ class FlutterMainMenuScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //TODO: Create main menu widget tree
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Flexible(
-          flex: 2,
-          child: Container(), //TODO: Replace with logo widget here
-        ),
-        Flexible(
-          flex: 1,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              //TODO: Put the first button widgets here
-              SizedBox(width: 10,),
-              //TODO: Put the second button widgets here
-            ],
+    return Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Flexible(
+            flex: 2,
+            child: Icon(Icons.account_circle, size: 300,), //TODO: Replace with logo here
           ),
-        ),
-      ],
-    );
+          SizedBox(height: 180,),
+          Flexible(
+            flex: 1,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                //TODO: Replace with first button widget
+                Text(
+                  "Start", 
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+                //TODO: Replace with second button widget
+                Text(
+                  "Game Settings",
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      );
   }
 }
