bb64e6484667b577dcc090d583b5afa1d44a7c64
GiantSweetroll <33750894+GiantSweetroll@users.noreply.github.com>
2021-02-12 16:22:52 +0700

    Added player spawn coordinates

--- climate_calling/lib/game_screens/levels/ArcticLevel.dart
+++ climate_calling/lib/game_screens/levels/ArcticLevel.dart
@@ -5,6 +5,9 @@ import 'package:flutter/src/gestures/tap.dart';
 
 class ArcticLevel extends BaseLevel {
 
+  //Constructor
+  ArcticLevel() : super(250, 250);
+
   //Overridden Methods
   @override
   bool isLevelFinished() {
@@ -13,22 +16,22 @@ class ArcticLevel extends BaseLevel {
   }
 
   @override
-  void onTapDown(TapDownDetails details) {
-      // TODO: implement onTapDown
-    }
-  
-    @override
-    void render(Canvas canvas) {
-      // TODO: implement render
-    }
-  
-    @override
-    void resize(Size size) {
-    // TODO: implement resize
+  void onTapDown(TapDownDetails details, Function fn) {
+    // TODO: implement onTapDown
   }
 
   @override
-  void update() {
+  void render(Canvas canvas) {
+    // TODO: implement render
+  }
+
+  @override
+  void resize(Size size) {
+  // TODO: implement resize
+  }
+
+  @override
+  void update(double t) {
     // TODO: implement update
   }
 }
