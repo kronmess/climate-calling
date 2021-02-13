c2503f2159c4092e5dfb8a1b0200c8e105c57e22
GiantSweetroll <33750894+GiantSweetroll@users.noreply.github.com>
2021-02-13 18:22:55 +0700

    Create Platform.dart

---
+++ climate_calling/lib/controllers/sprites/Platform.dart
@@ -0,0 +1,13 @@
+import 'package:climate_calling/controllers/sprites/Terrain.dart';
+import 'package:flame/sprite.dart';
+
+class Platform extends Terrain {
+  //Constructor
+  Platform(List<Sprite> sprites) : super(sprites, false);
+
+  //Overrridden Methods
+  @override
+  void onPassThrough(double t) {
+    // TODO: implement onPassThrough
+  }
+}
