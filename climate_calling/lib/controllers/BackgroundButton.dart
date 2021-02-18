ab6503c5982aa348a4b50eaa6dbfda62222b79f6
GiantSweetroll <33750894+GiantSweetroll@users.noreply.github.com>
2021-02-18 21:21:26 +0700

    Improved upon text placement and image sizing

--- climate_calling/lib/controllers/BackgroundButton.dart
+++ climate_calling/lib/controllers/BackgroundButton.dart
@@ -2,13 +2,22 @@ import "package:flutter/material.dart";
 
 class BackgroundButton extends GestureDetector {
   //Constructor
-  BackgroundButton({@required AssetImage image, String text, Function onTap, Color color = Colors.black}) : super(
+  BackgroundButton(
+    {
+      @required AssetImage image, 
+      String text, 
+      Function onTap,
+      Color color = Colors.transparent,
+      EdgeInsets padding
+     }
+    ) : super(
       child: Container(
+        padding: padding ?? EdgeInsets.fromLTRB(50, 20, 50, 25),
         decoration: BoxDecoration(
           color: color,
           image: DecorationImage(
             image: image,
-            fit: BoxFit.cover,
+            fit: BoxFit.fill,
           ),
         ),
         child: Text(text ?? ""),
