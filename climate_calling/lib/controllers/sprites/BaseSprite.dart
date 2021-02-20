05b2684739832ae2fa13e9c18be496d8328cdae2
GiantSweetroll <33750894+GiantSweetroll@users.noreply.github.com>
2021-02-20 08:46:04 +0700

    Added method to move the sprite

--- climate_calling/lib/controllers/sprites/BaseSprite.dart
+++ climate_calling/lib/controllers/sprites/BaseSprite.dart
@@ -9,17 +9,23 @@ import 'package:flutter/gestures.dart';
 class BaseSprite extends BaseTimedWidget {
   //Fields
   AnimationComponent _animationComponent;
-  double xVelocity, yVelocity;
+  double xVelocity, yVelocity, gravity;
 
   //Constructor
-  BaseSprite(List<Sprite> sprites) {
+  BaseSprite(List<Sprite> sprites, {
+    this.xVelocity = 5, 
+    this.yVelocity = 5, 
+    this.gravity = 0,
+  }) {
     this._animationComponent = AnimationComponent(0, 0, Animation.spriteList(sprites, stepTime: 0.1));
-    this.xVelocity = 0;
-    this.yVelocity = 0;
   }
 
   //Public Methods
   AnimationComponent getAnimationComponent() => _animationComponent;
+  void moveLeft() => this._animationComponent.x -= this.xVelocity;
+  void moveRight() => this._animationComponent.x += this.xVelocity;
+  void moveUp() => this._animationComponent.y -= this.yVelocity;    //Moving up is negative because of how the pixel coordinate works
+  void applyGravity() => this._animationComponent.y += this.yVelocity;    //Going down is positive because of how the pixel coordinate works
 
   //Overridden Methods
   @override
