38a3104eb1c8278d96b1a1cece3f26c8b60853c3
GiantSweetroll <33752872+GiantSweetroll@users.noreply.github.com>
2021-02-28 15:42:05 +0700

    started implementing  logic

--- climate_calling/lib/services/Camera.dart
+++ climate_calling/lib/services/Camera.dart
@@ -1,20 +1,71 @@
+import 'dart:math';
 import 'dart:ui';
 
-import 'package:climate_calling/controllers/BaseWidget.dart';
 import 'package:climate_calling/controllers/sprites/BaseSprite.dart';
-import 'package:flutter/src/gestures/tap.dart';
+import 'package:climate_calling/controllers/sprites/Player.dart';
+import 'package:flutter/cupertino.dart';
 
 class Camera{
   //Fields
-  List<BaseSprite> sprites;
-
+  List<BaseSprite> sprites = List();
+  double x, y;
+  Size maxSize, phoneSize;
+  Player player;
 
   //Constructors
-  Camera(this.sprites) {
+  Camera(this.player, {@required this.phoneSize, @required this.maxSize, this.sprites}) {
+    //Attempt to fix max camera size
+    if (this.maxSize.width < this.phoneSize.width) this.maxSize = Size(this.phoneSize.width, this.maxSize.height);
+    if (this.maxSize.height < this.phoneSize.height) this.maxSize = Size(this.maxSize.width, this.phoneSize.height);
+
+    //Determine camera center
+    this.x = 0;
+    this.y = 0;
+    this._updateCameraPos();
   }
 
   //Public methods
+  void addSprite(BaseSprite sprite) {
+    this.sprites.add(sprite);
+  }
+  void addSprites(List<BaseSprite> sprites) {
+    this.sprites.addAll(sprites);
+  }
+  void setSprites(List<BaseSprite> sprites) {
+    this.sprites = sprites;
+  }
+  List<BaseSprite> getSprites() => this.sprites;
   void update() {
-    // TODO: implement update
+    Point delta = this._updateCameraPos();    //Update camera position and get delta
+  }
+
+  //Private methods
+  Point _updateCameraPos() {
+    double xDelta;
+    double yDelta;
+    Point playerCenter = Point((player.getAnimationComponent().x + player.getAnimationComponent().width)/2, 
+                                (player.getAnimationComponent().y + player.getAnimationComponent().height)/2);
+    Point phoneCenter = Point(this.phoneSize.width/2, this.phoneSize.height);
+    //Determine delta
+    xDelta = playerCenter.x - this.x;
+    yDelta = playerCenter.y - this.y;
+
+    //Update camera x position
+    if (playerCenter.x < phoneCenter.x) this.x = phoneCenter.x;
+    else if (this.phoneSize.width < this.maxSize.width && playerCenter.x > phoneCenter.x) {
+      if (playerCenter.x <= this.maxSize.width - phoneCenter.x) this.x = playerCenter.x;        
+      else this.x = this.maxSize.width - phoneCenter.x;
+    }
+    else this.x = playerCenter.x;
+
+    //Update camera y position
+    if (playerCenter.y < phoneCenter.y) this.y = phoneCenter.y;
+    else if (this.phoneSize.height < this.maxSize.height && playerCenter.y > phoneCenter.y) {
+      if (playerCenter.y <= this.maxSize.height - phoneCenter.y) this.y = playerCenter.y;        
+      else this.y = this.maxSize.height - phoneCenter.y;
+    }
+    else this.y = playerCenter.y;
+    
+    return Point(xDelta, yDelta);
   }
 }
