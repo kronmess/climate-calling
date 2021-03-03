04ceaba80628df357ebc7f6df1c9f72ebdeb40ac
GiantSweetroll <33750894+GiantSweetroll@users.noreply.github.com>
2021-03-03 20:30:17 +0700

    Updated arctic tile reference to file name change

--- climate_calling/lib/shared/constants.dart
+++ climate_calling/lib/shared/constants.dart
@@ -16,4 +16,4 @@ const String PATH_ARCTIC_LEVEL_BG = "arctic level final 2.png",
               PATH_POLAR_BEAR_LEFT = "sprites/Polar Bear/left/",
               PATH_POLAR_BEAR_RIGHT = "sprites/Polar Bear/right/",
               PATH_IGLOO = "assets/images/sprites/igloo/igloo.png",
-              PATH_ARCTIC_TILE = "assets/images/sprites/arctic_level/tiles/ice_tile.png";
+              PATH_ARCTIC_TILE = "assets/images/sprites/arctic_level/tiles/ice_tiles.png";
