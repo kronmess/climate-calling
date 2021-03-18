bad40f4e1f6171606a1a19035d76255debdc7f05
GiantSweetroll <33752872+GiantSweetroll@users.noreply.github.com>
2021-03-18 19:55:07 +0700

    finished with settings page i think

--- climate_calling/lib/flutter_screens/FlutterGameSettings.dart
+++ climate_calling/lib/flutter_screens/FlutterGameSettings.dart
@@ -24,120 +24,124 @@ class _FlutterGameSettingsScreenState extends State<FlutterGameSettingsScreen> {
             image: AssetImage(PATH_SETTINGS_TILE),
           ),
         ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: Stack(
           children: [
-            Flexible(
-              flex: 1,
-              child: Align(
-                alignment: Alignment.topCenter,
-                child: Container(
-                  padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage(PATH_BUTTON_MAIN_MENU),
-                      fit: BoxFit.fill
+            Align(
+              alignment: Alignment.topCenter,
+              child: Container(
+                padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(PATH_BUTTON_MAIN_MENU),
+                    fit: BoxFit.fill
+                  ),
+                ),
+                child: Text(
+                  "Settings",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 50, 0, 0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // SizedBox(height: 20.0,),
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage(PATH_NOTO_FIRE),
+                        fit: BoxFit.fitHeight
+                      ),
+                    ),
+                    child: Text("Background Music",
+                              style: TextStyle(fontWeight: FontWeight.bold)),
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image(
+                        image: AssetImage(PATH_SOUND),
+                        height: 30,
+                        width: 30,
+                      ),
+                      Slider(
+                          value: _value,
+                          min: 0,
+                          max: 100,
+                          activeColor: Colors.black,
+                          inactiveColor: Colors.grey,
+                          label: _valuetwo.round().toString(),
+                          onChanged: (double newValue) {
+                            setState(() {
+                              _value = newValue;
+                            });
+                          }),
+                    ],
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage(PATH_NOTO_FIRE),
+                        fit: BoxFit.fitHeight
+                      ),
+                    ),
+                    child: Text("Sound Effects",
+                              style: TextStyle(fontWeight: FontWeight.bold)),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image(
+                        image: AssetImage(PATH_SOUND),
+                        height: 30,
+                        width: 30,
+                      ),
+                      Slider(
+                        value: _valuetwo,
+                        min: 0,
+                        max: 100,
+                        activeColor: Colors.black,
+                        inactiveColor: Colors.grey,
+                        label: _valuetwo.round().toString(),
+                        onChanged: (double newValue) {
+                          setState(() {
+                            _valuetwo = newValue;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage(PATH_SETTINGS_BORDER),
+                        fit: BoxFit.fill,
+                      )
+                    ),
+                    child: SettingsButton(
+                      image: AssetImage(PATH_SETTINGS_BUTTON),
+                      text: "Return to Main Menu",
+                      onTap: () {
+                        climateCalling.switchScreen(ScreenState.kMainMenu);
+                      },
                     ),
                   ),
-                  child: Text(
-                    "Settings",
-                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                  // SizedBox(height: 5.0,),
+                  Image(
+                    image: AssetImage(PATH_LOGO_APP),
+                    height: 40,
+                    width: 40,
                   ),
-                ),
+                ],
               ),
-            ),
-            // SizedBox(height: 20.0,),
-            Container(
-              padding: EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage(PATH_NOTO_FIRE),
-                  fit: BoxFit.fitHeight
-                ),
-              ),
-              child: Text("Background Music",
-                        style: TextStyle(fontWeight: FontWeight.bold)),
-            ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image(
-                  image: AssetImage(PATH_SOUND),
-                  height: 30,
-                  width: 30,
-                ),
-                Slider(
-                    value: _value,
-                    min: 0,
-                    max: 100,
-                    activeColor: Colors.black,
-                    inactiveColor: Colors.grey,
-                    label: _valuetwo.round().toString(),
-                    onChanged: (double newValue) {
-                      setState(() {
-                        _value = newValue;
-                      });
-                    }),
-              ],
-            ),
-            Container(
-              padding: EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage(PATH_NOTO_FIRE),
-                  fit: BoxFit.fitHeight
-                ),
-              ),
-              child: Text("Sound Effects",
-                        style: TextStyle(fontWeight: FontWeight.bold)),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image(
-                  image: AssetImage(PATH_SOUND),
-                  height: 30,
-                  width: 30,
-                ),
-                Slider(
-                  value: _valuetwo,
-                  min: 0,
-                  max: 100,
-                  activeColor: Colors.black,
-                  inactiveColor: Colors.grey,
-                  label: _valuetwo.round().toString(),
-                  onChanged: (double newValue) {
-                    setState(() {
-                      _valuetwo = newValue;
-                    });
-                  },
-                ),
-              ],
-            ),
-            Container(
-              padding: EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage(PATH_SETTINGS_BORDER),
-                  fit: BoxFit.fill,
-                )
-              ),
-              child: SettingsButton(
-                image: AssetImage(PATH_SETTINGS_BUTTON),
-                text: "Return to Main Menu",
-                onTap: () {
-                  climateCalling.switchScreen(ScreenState.kMainMenu);
-                },
-              ),
-            ),
-            // SizedBox(height: 5.0,),
-            Image(
-              image: AssetImage(PATH_LOGO_APP),
-              height: 40,
-              width: 40,
             ),
           ],
         ),
