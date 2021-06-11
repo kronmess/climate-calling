48dd522785319da506f90bfc10e6d1d03c1a08fc
GiantSweetroll <33752872+GiantSweetroll@users.noreply.github.com>
2021-06-11 08:40:31 +0700

    Create ForestLevel.dart

---
+++ climate_calling/lib/game_screens/levels/ForestLevel.dart
@@ -0,0 +1,19 @@
+import 'package:climate_calling/game_screens/levels/BaseLevel.dart';
+
+class ForestLevel extends BaseLevel {
+
+  // Constructor
+  ForestLevel(double x, double y) : super(x, y);
+
+  // Overridden Methods
+  @override
+  void initPlatforms() {
+    // TODO: implement initPlatforms
+  }
+
+  @override
+  void initTerrain() {
+    // TODO: implement initTerrain
+  }
+
+}
